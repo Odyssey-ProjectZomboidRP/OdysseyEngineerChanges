613c550d9c6f6a6452502b599ad38d5345b64ce7
TRNEEDANAME <trneedaname@proton.me>
2025-02-18 13:38:15 +0100

    Started Metal Strips

--- Contents/mods/OdysseyEngineer/media/lua/client/ISUI/TOC_MetalPatch.lua
+++ Contents/mods/OdysseyEngineer/media/lua/client/ISUI/TOC_MetalPatch.lua
@@ -20,10 +20,19 @@ ISInventoryPaneContextMenu.doClothingPatchMenu = function(player, clothing, cont
     local fabric1 = playerObj:getInventory():getItemFromType("RippedSheets", true, true)
     local fabric2 = playerObj:getInventory():getItemFromType("DenimStrips", true, true)
     local fabric3 = playerObj:getInventory():getItemFromType("LeatherStrips", true, true)
-    local fabric4 = playerObj:getInventory():getItemFromType("MetalStrips", true, true)
+    if (SandboxVars.TOC.EnableMetalPatches) then
+        local fabric4 = playerObj:getInventory():getItemFromType("MetalStrips", true, true)
+    end
 
 
-    if not thread or not needle or (not fabric1 and not fabric2 and not fabric3 and not fabric4) then
+    if not thread or not needle or (not fabric1 and not fabric2 and not fabric3 and not fabric4 and SandboxVars.TOC.EnableMetalPatches) then
+        local patchOption = context:addOption(getText("ContextMenu_Patch"))
+        patchOption.notAvailable = true
+        local tooltip = ISInventoryPaneContextMenu.addToolTip()
+        tooltip.description = getText("ContextMenu_CantRepair")
+        patchOption.toolTip = tooltip
+        return
+    elseif not thread or not needle or (not fabric1 and not fabric2 and not fabric3 and SandboxVars.TOC.EnableMetalPatches ~= false) then 
         local patchOption = context:addOption(getText("ContextMenu_Patch"))
         patchOption.notAvailable = true
         local tooltip = ISInventoryPaneContextMenu.addToolTip()
