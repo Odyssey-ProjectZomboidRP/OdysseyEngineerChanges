1ec23f4b1d988967199b2b8cae0d8ee61ceb5c1c
TRNEEDANAME <trneedaname@proton.me>
2025-02-24 09:53:05 +0100

    added more

--- Contents/mods/OdysseyEngineer/media/lua/client/TOCEngineer.lua
+++ Contents/mods/OdysseyEngineer/media/lua/client/TOCEngineer.lua
@@ -36,13 +36,13 @@ end
 
 ---@param weapon HandWeapon
 --- Make the gun jam, reduce condition by `SandboxVars.TOC.ConditionReduction` amount
-function SetGunStuff(weapon)
+function SetGunStuff(weapon, value)
     --- Set the gun to be jammed
     weapon:setJammed(true)
     local condition = weapon:getCondition()
     --- If the condition is less than the reduction, we set the condition to 0 to avoid fuckery
     if (condition < SandboxVars.TOC.ConditionReduction) then
-        weapon:setCondition(0)
+        weapon:setCondition(0)SetGunStuff
     else
         weapon:setCondition(condition - SandboxVars.TOC.ConditionReduction)
     end
@@ -52,7 +52,7 @@ end
 ---@param zombie IsoZombie
 --[[ We get the `aimLevel` and `reloadLevel` of the player, the pos of said player, and the pos of the zed we hit.
 We then use `SandboxVars.TOC.ReloadLevel` to check if the player is high enough to only have `SandboxVars.TOC.ChanceToJam` to jam
-Else, garanteed chance of jam & reduced condition as set by the function `SetGunStuff(weapon)`.
+Else, garanteed chance of jam & reduced condition as set by the function `SetGunStuff(weapon)`. The condition is reduced by `SandboxVars.TOC.ConditionReduction` and `SandboxVars.TOC.ConditionReductionSkillBelow` is added if the skill is too low, else `SandboxVars.TOC.ConditionReductionSkillHigher`
 This also use `SandboxVars.TOC.AimingLevel` to check wether the fire start at `PlayerPos` or `ZombiePos`]]--
 function StartFunc(player, zombie)
     --- We check the aiming level
@@ -71,12 +71,12 @@ function StartFunc(player, zombie)
     if (TOCCheckGun(player, weapon) and CorrectAmmoType(weapon)) then
         if (reloadLevel < SandboxVars.TOC.ReloadLevel) then
             --- We set the gun to be jammed
-            SetGunStuff(player:getPrimaryHandItem())
+            SetGunStuff(player:getPrimaryHandItem(), SandboxVars.TOC.ConditionReductionSkillBelow)
         --- Else, only a certain chance to jamm
         else
             local HundredChance = ZombRand(100)
             if (HundredChance <= SandboxVars.TOC.ChanceToJam) then
-                SetGunStuff(player:getPrimaryHandItem())
+                SetGunStuff(player:getPrimaryHandItem(), SandboxVars.TOC.ConditionReductionSkillHigher)
             end
         end
 
